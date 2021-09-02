5cd1fe6767d972ce67d8d8f2cd51dadd2e36f2bf
Diane DELORD <82445395+DianeDelord@users.noreply.github.com>
2021-09-02 17:59:26 +0200

    refacto de l'affichage des cardPhotographers

---
+++ photographer.js
@@ -0,0 +1,28 @@
+fetch("assets/data.json")
+    .then(response => {
+        if (response.ok) {
+            console.log("ok");
+            return response.json();
+        } else {
+            console.log("erreur avec Jason");
+            console.log("erreur de chargement des données")
+        }
+    })
+    .then((data) => {
+        console.log(data, data.photographers);
+    })
+
+function toPascalCase(string) {
+    return `${string}`
+        .replace(new RegExp(/[-_]+/, 'g'), ' ')
+        .replace(new RegExp(/[^\w\s]/, 'g'), '')
+        .replace(
+            new RegExp(/\s+(.)(\w+)/, 'g'),
+            ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
+        )
+        .replace(new RegExp(/\s/, 'g'), '')
+        .replace(new RegExp(/\w/), s => s.toUpperCase());
+}
+
+let informations = document.getElementById("informations");
+informations.innerHTML = ("coucou");
