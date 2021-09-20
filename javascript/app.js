7ff4d69c264b43431010e3b1d00f30ccc85ffdfb
Diane DELORD <82445395+DianeDelord@users.noreply.github.com>
2021-09-20 11:09:10 +0200

    refacto POO index.html

--- javascript/app.js
+++ javascript/app.js
@@ -5,10 +5,11 @@ let resultOfSearch = document.getElementById('resultOfSearch');
 //////////// scroll ////////////////
 // https://www.w3schools.com/jsref/event_onscroll.asp
 // https://stackoverflow.com/questions/31223341/detecting-scroll-direction
+
 // quand on scrolle, le topLink apparaît
 let topLink = document.querySelector("#topLink");
 
-function scrollDetect() {
+function scrollAppear() {
     var lastScroll = 0;
     window.onscroll = function() {
         let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
@@ -16,15 +17,15 @@ function scrollDetect() {
         if (currentScroll > 0 && lastScroll <= currentScroll) {
             lastScroll = currentScroll;
             topLink.setAttribute('class', 'show');
-            console.log(" toplink down");
+            console.log("toplink down");
         } else {
             lastScroll = currentScroll;
             topLink.setAttribute('class', 'hide');
-            console.log(" toplink up");
+            console.log("toplink up");
         }
     };
 }
-scrollDetect();
+scrollAppear();
 
 /////////////////////////////////////
 /////////////////////////////////////
@@ -47,7 +48,7 @@ fetch("assets/data.json")
         console.log(data.photographers);
         return data;
     });
-
+/*
 // fonction asynchrone https://dmitripavlutin.com/javascript-fetch-async-await/
 async function createPhotographerCard() {
     const response = await fetch("assets/data.json");
@@ -80,7 +81,7 @@ createPhotographerCard()
         }
     })
 
-
+*/
 
 
 ////////////////////////////////////////////
@@ -97,53 +98,9 @@ function toPascalCase(string) {
         .replace(new RegExp(/\w/), s => s.toUpperCase());
 }
 
-//////////////////////////////////////////////////
-// input setup
-//searchInput.addEventListener('input', (e) => {
-//    searchTerm = e.target.value;
-//    console.log(e.target.value);
-//    showPhotographers();
-//});
 
 // create url //
 // https://www.youtube.com/watch?v=6BozpmSjk-Y
-
-
-// filtre au clic des tags
-
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-const result = words.filter(word => word.length > 6);
-console.log(result);
-
-async function tagFilter() {
-    const response = await fetch("assets/data.json");
-    const data = await response.json();
-    return data;
-}
-
-// function remplissage de la page grâce aux données récupérées depuis data.json
-tagFilter()
-    .then(data => {
-        console.log(data.media);
-        let mediasToFilter = data.media;
-
-        for (let media of mediasToFilter) {
-            console.log(media);
-            console.log(mediasToFilter);
-        }
-        let mediasFiltres;
-        mediasFiltres = mediasToFilter.filter(function(media) {
-            if (media.tags == 'events') {
-                console.log("j'ai trouvé la photo nommée " + media.image + " du photographe avec l'id " + media.photographerId);
-            }
-            console.log(mediasFiltres);
-        });
-    })
-
-// var mediasRecup = data.media;
-//  var tri = mediasRecup.filter(function(media) {
-//      return media.photographerId == resultPh[0].id;
-//   });
 
 
 // ressources qui m'ont été utiles
@@ -154,62 +111,119 @@ tagFilter()
 // https://www.youtube.com/watch?v=b0dPBK37-M8&t=24s
 
 
+// fonction asynchrone https://dmitripavlutin.com/javascript-fetch-async-await/
+async function createPhotographerCard2() {
+    const response = await fetch("assets/data.json");
+    const data = await response.json();
+    return data;
+}
+
+// function remplissage de la page grâce aux données récupérées depuis data.json
+createPhotographerCard2()
+    .then(data => {
+        console.log(data)
+        console.log(data.photographers)
+        console.log(data.photographers[0].city)
+
+        let resultOfSearch = document.getElementById("resultOfSearch");
+        let photographerCard_ul = document.createElement("ul");
+        resultOfSearch.appendChild(photographerCard_ul)
+
+        for (let photographer of data.photographers) {
+            console.log(photographer);
+
+            // variables de données
+            let photographer_city = photographer.city;
+            let photographer_country = photographer.country;
+            let photographer_id = photographer.id;
+            let photographer_name = photographer.name;
+            let photographer_portrait = photographer.portrait;
+            let photographer_price = photographer.price;
+            let photographer_tagline = photographer.tagline;
+            let photographer_tags = photographer.tags;
+
+            // creation des éléments HTML du DOM
+            let photographerCard_a = document.createElement("a");
+            let photographerCard_li = document.createElement("li");
+            let photographerCard_img = document.createElement("img");
+            let photographerCard_h2 = document.createElement("h2");
+            let photographerCard_location = document.createElement("p");
+            let photographerCard_tagline = document.createElement("p");
+            let photographerCard_price = document.createElement("p");
+            let photographer_tagsList = document.createElement("p")
+
+            photographerCard_ul.setAttribute("id", "selected-items");
+            photographerCard_a.setAttribute("id", `${photographer_id}`);
+            photographerCard_a.setAttribute("class", "restrict");
+            photographerCard_a.setAttribute("href", `/photographer.html?id=${photographer_id}`);
+            photographerCard_li.setAttribute("class", "photographers-items");
+            photographerCard_img.setAttribute("class", "photographer-portrait");
+            photographerCard_img.setAttribute("src", `images/Sample Photos/Photographers ID Photos/${(toPascalCase(photographer_name) + '.jpg')}`);
+            photographerCard_img.setAttribute("alt", `${photographer_name}`);
+            photographerCard_h2.setAttribute("class", "photographer-name");
+            photographerCard_h2.textContent = photographer_name;
+            photographerCard_location.setAttribute("class", "photographer-city");
+            photographerCard_location.textContent = photographer_city + ", " + photographer_country;
+            photographerCard_tagline.setAttribute("class", "tagline");
+            photographerCard_tagline.textContent = photographer_tagline;
+            photographerCard_price.setAttribute("class", "photogrpaherPrice");
+            photographerCard_price.textContent = photographer_price + "€/jour";
+            photographerCard_price.setAttribute("class", "photographerPrice");
+            photographer_tagsList.setAttribute("class", "photographer_listOfTags");
 
 
+            console.log(photographerCard_ul);
+            console.log(photographer_portrait);
+            console.log(photographer_tags);
 
-/* 
+            let photographer_tagsA
 
-   let photographerCity = photographer.city;
-            let photographerCountry = photographer.country;
-            let photographerId = photographer.id;
-            let photographerName = photographer.name;
-            let photographerPortrait = photographer.portrait;
-            let photographerPrice = photographer.price;
-            let photographerTagline = photographer.tagline;
-            let photographerTags = photographer.tags;
+            for (let i = 0; i < photographer_tags.length; i++) {
+                photographer_tagsA = document.createElement("a");
+                photographer_tagsA.setAttribute("class", "lienTag");
+                let eachTag = document.createElement("h5");
+                eachTag.setAttribute("class", "generatedTags")
+                eachTag.textContent = `#${photographer_tags[i]}`;
+                photographer_tagsA.appendChild(eachTag);
+                photographer_tagsList.appendChild(photographer_tagsA);
 
-            return {
-                photographerCity,
-                photographerCountry,
-                photographerId,
-                photographerName,
-                photographerPortrait,
-                photographerPrice,
-                photographerTagline,
-                photographerTags,
-                createCard,
+                console.log(photographer_tagsA);
+                console.log(i);
+                console.log(eachTag);
             }
-            function createCard(photographer) {
-                // creation des éléments HTML du DOM
-                let photographerCard_ul = document.createElement("ul");
-                let photographerCard_a = document.createElement("a");
-                let photographerCard_li = document.createElement("li");
-                let photographerCard_img = document.createElement("img");
-                let photographerCard_h2 = document.createElement("h2");
-                let photographerCard_p = document.createElement("p");
 
-                photographerCard_ul.setAttribute("id", "selected-items");
-                photographerCard_ul.appendChild(resultOfSearch);
-                photographerCard_a.setAttribute("id", `${photographerId}`);
-                photographerCard_a.setAttribute("class", "restrict");
-                photographerCard_a.setAttribute("href", `/photographer.html?id=${photographerId}`);
-                photographerCard_li.setAttribute("class", "photographers-items");
-                photographerCard_img.setAttribute("class", "photographer-portrait");
-                photographerCard_img.setAttribute("src", `images/Sample Photos/Photographers ID Photos/${(toPascalCase(photographerName) + '.jpg')}`);
-                photographerCard_img.setAttribute("alt", `${photographerName}`);
-                photographerCard_h2.setAttribute("class", "photographer-name");
-                photographerCard_h2.textContent = `photographerName`;
-                photographerCard_p.setAttribute("class", "photographer-city");
-                photographerCard_p.textContent = photographerCity + ", " + photographerCountry;
-                photographerCard_p.textContent = (photographerPortrait + ", " + photographerPrice + ", " + photographerTagline + ", " + photographerTags);
-                return {
-                    photographerCard_ul,
-                    photographerCard_a,
-                    photographerCard_li,
-                    photographerCard_img,
-                    photographerCard_h2,
-                    photographerCard_p,
-                }
-            }
+            photographerCard_ul.appendChild(photographerCard_a)
+            photographerCard_a.appendChild(photographerCard_li)
+            photographerCard_li.appendChild(photographerCard_img)
+            photographerCard_li.appendChild(photographerCard_h2)
+            photographerCard_li.appendChild(photographerCard_location)
+            photographerCard_li.appendChild(photographerCard_tagline)
+            photographerCard_li.appendChild(photographerCard_price)
+            photographerCard_li.appendChild(photographer_tagsList)
+                // photographer_tagsList.appendChild(photographer_tagsA)
+        }
+        //initialiseDOM()
 
-            */
+
+        /*
+                    function card
+                    // tags du photographe
+                    const createHashtag = document.createElement("section");
+                        createHashtag.setAttribute("class", "tagline")
+                        for (let i = 0; i < photographer.length; i++) {
+                            const photographerTags = photographer["photographers"][i]["tags"];
+            
+                            for (let t = 0; t < photographerTags.length; t++) {
+            
+                                const tagLink = document.createElement("a");
+                                const span = document.createElement("h4");
+                                span.textContent = `#${photographer.tagline[t].toLowerCase()}`;
+                                document.createElement("article").classList.add(`${photographer.tagline[t].toLowerCase()}`);
+                                document.createElement("section").appendChild(tagLink);
+                                tagLink.appendChild(span);
+                                console.log("photographecoucou");
+            
+                            }
+            
+                        }*/
+    })
--- javascript/filter.js
+++ javascript/filter.js
@@ -1,138 +1,88 @@
 console.log("filter connecté")
 
+/*
+// filtre au clic des tags
 
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const result = words.filter(word => word.length > 6);
+console.log(result);
+*/
+/*
+async function tagFilter() {
+    const response = await fetch("assets/data.json");
+    const data = await response.json();
+    return data;
+}
+*/
+
+/*
+// function remplissage de la page grâce aux données récupérées depuis data.json
+tagFilter()
+    .then(data => {
+        console.log(data.media);
+        let mediasToFilter = data.media;
+
+        for (let media of mediasToFilter) {
+            console.log(media);
+            console.log(mediasToFilter);
+        }
+        let mediasFiltres;
+        mediasFiltres = mediasToFilter.filter(function(media) {
+            if (media.tags == 'events') {
+                console.log("j'ai trouvé la photo nommée " + media.image + " du photographe avec l'id " + media.photographerId);
+            }
+            console.log(mediasFiltres);
+        });
+    })*/
+
+/*
+// var mediasRecup = data.media;
+//  var tri = mediasRecup.filter(function(media) {
+//      return media.photographerId == resultPh[0].id;
+//   });
+////////////////////////
+*/
+
+/*
+let clickedHashtag;
+//let resultOfSearch = document.getElementById("resultOfSearch");
 class filtrage {
     static init() { // créer un comportement à appliquer à chaque lien
-        const links = Array.from(document.querySelectorAll('a[href$=".jpg"]'));
-        const gallery = links.map(link => link.getAttribute('href'));
-        console.log(links)
-        console.log(links[0])
-        console.log(links[0])
-        console.log(gallery);
+        const collectHashtags = Array.from(document.querySelectorAll('a[href^="#"]'));
+
+        console.log(collectHashtags)
+        console.log(collectHashtags[1].innerHTML);
         console.log("filtrage ok")
 
-        links.forEach(link => link.addEventListener('click', e => {
+        let triage = document.querySelectorAll(".restrict");
+        console.log(triage);
+        //  console.log(triage[0].outerText); // je dois chercher là dedans qu'il y ait clickedHashtag.substr(1)
+
+        /* collectHashtags.forEach(tag => tag.addEventListener('click', e => {
             e.preventDefault()
-            new filtrage(e.currentTarget.getAttribute('href'), gallery); // récupérer url     
-            console.log("filtrage init ok")
+            new filtrage(e.currentTarget.getAttribute('href'), collectHashtags); // récupérer url     
+            clickedHashtag = e.currentTarget.innerHTML
+            console.log(clickedHashtag) // tag sur lequel on a clické
+                // return clickedHashtag
+
+            //sentence.includes(word)
+            let element
+
+            function resultatTag() {
+                triage.forEach(
+                    function(currentValue, currentIndex, listObj) {
+                        console.log(currentValue + ', ' + currentIndex + ', ' + this);
+                    },
+                    'myThisArg'
+                );
+            }
+            resultatTag()
+            resultOfSearch.innerHTML = element;
         }))
-    }
-
-    /**
-     * @param {string} url URL de l'image
-     * * @param {string[]} images chemins des images de la filtrage
-     */
-    constructor(url, images) {
-        this.element = this.buildDOM(url);
-        console.log(this.element)
-
-        this.images = images;
-        console.log(images)
-
-        this.loadImage(url);
-        this.onKeyUp = this.onKeyUp.bind(this);
-        document.body.appendChild(this.element);
-        document.addEventListener('keyup', this.onKeyUp);
-    }
-
-
-    /**
-     * @param {string} url URL de l'image
-     */
-    loadImage(url) {
-        this.url = null;
-        /*
-        if (url.indexOf('mp4')) {
-            console.log("c'est une vidéo, pas une image!");
-        }
-        */
-        console.log("salut")
-
-        const image = new Image();
-        const container = this.element.querySelector('.lightbox__container')
-        const loader = document.createElement('div')
-        loader.classList.add('lightbox__loader');
-        loader.setAttribute("src", "images/loader.svg");
-        container.innerHTML = '';
-        container.appendChild(loader)
-        image.onload = () => {
-            container.removeChild(loader);
-            container.appendChild(image);
-            this.url = url;
-        }
-        image.src = url;
-    }
-
-    onKeyUp(e) {
-        if (e.key == 'Escape') {
-            this.close(e);
-        } else if (e.key == 'ArrowLeft') {
-            this.prev(e)
-        } else if (e.key == 'ArrowRight') {
-            this.next(e)
-        }
-    }
-
-    // croix qui ferme la lightbox
-    close(e) {
-        e.preventDefault();
-        this.element.classList.add('fadeOut');
-        window.setTimeout(() => {
-            this.element.parentElement.removeChild(this.element);
-        }, 500);
-        document.removeEventListener('keyup', this.onKeyUp);
-    }
-
-    // on conserve l'url de l'image affichée afin de 
-    // pouvoir retrouver la position de cette image dans notre 
-    // galerie pour la navigation next et prev.
-    /**
-     * @param {MouseEvent|KeyboardEvent} e 
-     */
-    next(e) {
-        e.preventDefault()
-        let i = this.images.findIndex(image => image === this.url);
-        if (i === this.images.length - 1) {
-            i = -1 // si on est arrivé à la dernière image, on repart du début
-        }
-        this.loadImage(this.images[i + 1]);
-    }
-
-    /**
-     * @param {MouseEvent|KeyboardEvent} e 
-     */
-    prev(e) {
-        e.preventDefault()
-        let i = this.images.findIndex(image => image === this.url);
-        if (i === 0) {
-            i = this.images.length;
-        }
-        this.loadImage(this.images[i - 1]);
-    }
-
-
-    /**
-     * @param {string} url URL de l'image
-     * @return {HTMLElement}
-     **/
-    buildDOM(url) { // retournera un élément html
-        const dom = document.createElement('div');
-        dom.classList.add('lightbox');
-        dom.innerHTML = `<button class="lightbox__close">Fermer</button>
-        <button class="lightbox__next">Suivant</button>
-        <button class="lightbox__prev">Précédent</button>
-        <div class="lightbox__container">
-        </div>`
-        dom.querySelector('.lightbox__close').addEventListener('click', this.close.bind(this));
-        dom.querySelector('.lightbox__next').addEventListener('click', this.next.bind(this));
-        dom.querySelector('.lightbox__prev').addEventListener('click', this.prev.bind(this));
-        console.log(url);
-        return dom;
     }
 }
 
 
-
 setTimeout(function() {
     filtrage.init();
-}, 2000);
+}, 2000);*/
